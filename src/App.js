3bc8b8493d0e692c09716820a4c00a67de0a57c8
LoperaSebas <sebastianparralopera351@gmail.com>
2020-08-01 00:27:05 -0500

    Semana 10 David

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>David</h1>
+      <h2>Tabla de integrantes de Pet Society</h2>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombres</th>
+              <th>Correo</th>
+              <th>Cedula</th>
+              <th>Celular</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Sebastián Parra Lopera</td>
+              <td>sebastianparralopera351@gmail.com</td>
+              <td>1011390733</td>
+              <td>3104109270</td>
+            </tr>
+            <tr>
+              <td>Joaquín Higuita López</td>
+              <td>Joaquin050218@gmail.com</td>
+              <td>1015066347</td>
+              <td>3206270582</td>
+            </tr>
+            <tr>
+              <td>David Castañeda Quintero</td>
+              <td>davidcastanedaquintero@gmail.com</td>
+              <td>1025641452</td>
+              <td>3052583246</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+    
   );
 }
 
